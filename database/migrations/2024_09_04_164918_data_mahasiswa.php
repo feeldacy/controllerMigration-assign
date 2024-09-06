fa994b871ea9d7d0b133f3937d59c3d37cc212ac
feeldacy <husnafelisacahyani@gmail.com>
2024-09-06 14:34:26 +0700

    #

--- database/migrations/2024_09_04_164918_data_mahasiswa.php
+++ database/migrations/2024_09_04_164918_data_mahasiswa.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('mahasiswa', function (Blueprint $table) {
@@ -20,10 +17,6 @@ return new class extends Migration
             $table->foreign('DosPem')->references('id')->on('dosen')->onDelete('cascade');  
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('mahasiswa');
